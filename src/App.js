64425b546a72bdc2a22fb2235f89a843483df4d3
Lydia <lydia.lawli@pickpack.de>
2020-03-20 12:43:36 +0100

    tabs component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import './App.css'
 import Search from "./components/Search"
+import Tabs from './components/Tabs'
 
 function App() {
   return (
     <div className="App">
-      <h1>Aloha!</h1>
       <Search />
+      <Tabs>
+        <div label="Pull requests">
+          <em>List of pull requests</em>
+      </div>
+        <div label="Open issues">
+          <em>List of open issues</em>
+      </div>
+        <div label="Closed issues">
+        <em>List of closed issues</em>
+      </div>
+      </Tabs>
     </div>
   )
 }

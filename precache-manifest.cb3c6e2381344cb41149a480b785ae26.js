f8acfa8152652c2f7b22c95e292ab4419abb5a3d
Lydia <lydia.lawli@pickpack.de>
2020-03-24 19:23:23 +0100

    Updates

---
+++ precache-manifest.cb3c6e2381344cb41149a480b785ae26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418d5b192b905f7e3c18762610b5c5db",
+    "url": "/git-repo-viewer/index.html"
+  },
+  {
+    "revision": "a4d16be9ed6747df756a",
+    "url": "/git-repo-viewer/static/css/main.39b18150.chunk.css"
+  },
+  {
+    "revision": "25caf9d047e8ec2f70c1",
+    "url": "/git-repo-viewer/static/js/2.4aeb3bce.chunk.js"
+  },
+  {
+    "revision": "9f55a1ac102148e41f3a9543b31fc30f",
+    "url": "/git-repo-viewer/static/js/2.4aeb3bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d16be9ed6747df756a",
+    "url": "/git-repo-viewer/static/js/main.7407b4ea.chunk.js"
+  },
+  {
+    "revision": "d58b2964f2a9c5cc7c04",
+    "url": "/git-repo-viewer/static/js/runtime-main.19580735.js"
+  }
+]);
